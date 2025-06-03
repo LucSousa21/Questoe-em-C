c33084f6beefda693abf7e1b9c51832817c20361
LucSousa21 <souza.lucas1805@gmail.com>
2025-06-02 23:54:14 -0300

    Commit inicial

---
+++ progrma-diferente/par-impa.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(){
+    system("chcp 65001");
+
+    int i, soma[3], ponto[3];
+    char nome[3][20];
+    
+    for(i = 0; i < 3; i ++){
+        printf("Aluno %d: \n", i + 1);
+        scanf(" %s", nome[i]);
+        for(int j = 0; j < 3; j++){
+            printf("Nota %d: \n", j + 1);
+            scanf(" %d", &ponto[j]);
+            
+        }
+        
+    }
+    system("cls");
+
+    for(i = 0; i < 3; i ++){
+        printf("Aluno %d %s - Nota final: %d\n ", i + 1, nome[i], soma[i]);
+                                     
+    }
+    /*for(i = 0; i < 3; i ++){
+        if(resposta[i] == gab[i]){
+            printf("Questão %d esta certa (gabarito %c)\n", i + 1, gab[i], questoes[i]);
+        } else{
+            printf("Questão %d esta errada (gabarito %c)\n", i + 1, gab[i], questoes[i]);
+        }
+    }*/
+    return 0;
+}
